7f17b7dd7eeef369abba90078f437a18a3aa9914
umairarshadbutt <umair6622@gmail.com>
2020-07-30 02:47:34 +0500

    update header post scrap

--- bin/main.rb
+++ bin/main.rb
@@ -11,6 +11,11 @@ require_relative '../lib/articles.rb'
 require_relative '../lib/display.rb'
 post_array = []
 
+doc.css('div .fi').each do |programming_post|
+  post = Articles.new(programming_post)
+  post_array.push(post)
+end
+
 doc.css('section .fg').each do |programming_post|
   post = Articles.new(programming_post)
   post_array.push(post)
