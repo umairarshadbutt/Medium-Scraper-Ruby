66b9f40888af0522713ade0d899edaf87b2a93d4
umairarshadbutt <umair6622@gmail.com>
2020-07-29 21:19:26 +0500

    Scrapping the content from page

--- bin/main.rb
+++ bin/main.rb
@@ -1,1 +1,15 @@
-puts 'Hello'
+#!/usr/bin/env ruby
+
+require 'nokogiri'
+require 'open-uri'
+
+html = 'https://medium.com/topic/programming'
+doc = Nokogiri::HTML(URI.open(html))
+
+doc.css('section .fg').each do |programming_post|
+  puts programming_post.css('h3.ap').text
+  puts ''
+  puts programming_post.css('div.ga .ch').text
+  puts ''
+  puts programming_post.css('div.gw').text
+end
