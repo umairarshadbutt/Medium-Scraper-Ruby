03c1bf730b4ec8f2c1e33272b63ba2618c800ea4
umairarshadbutt <umair6622@gmail.com>
2020-07-30 12:17:07 +0500

    Fix rubocop issues

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,3 @@
-
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
@@ -9,5 +8,4 @@ RSpec.configure do |config|
   end
 
   config.shared_context_metadata_behavior = :apply_to_host_groups
-
 end
